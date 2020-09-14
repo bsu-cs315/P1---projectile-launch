d304dff026a23f29f4d14bb30af166e13bd7f314
Janardhen Jayavel <52244557+Janardhen@users.noreply.github.com>
2020-09-14 00:04:23 -0400

    Scenes

---
+++ scenes/HUD.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+signal start_game
+
+func set_strength(value):
+	$StrengthBar.value = value
+
+func show_game_over():
+	$StartButton.show()
+	
+func hide_tutorial():
+	$RightArrow.hide()
+	$LeftArrow.hide()
+	$SpaceBar.hide()
+	
+func set_projectile_remaining(count):
+	$InventoryLabel.text = str(count) + " Projectiles Remaining"
+	
+func increase_score():
+	$ScoreLabel.text = str(int($ScoreLabel.text) + 50)
+	$ScoreInventory.text = str(int($ScoreInventory.text)
+	+1)
+
+func _on_Button_pressed():
+	emit_signal("start_game")
+
+
---
+++ scenes/Label.gd
@@ -0,0 +1,6 @@
+extends Label
+
+
+func _ready():
+	set_text("Use left and right arrows to control angle  and Spacebar to launch \n Hit the aliens to score points")
+
